cf7da765a0e8d46177fc53ed7017a6101e49dc26
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-02-26 14:09:30 +0700

    tets

--- testgit/ContentView.swift
+++ testgit/ContentView.swift
@@ -10,14 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-            HStack {
-                Image(systemName: "microphone.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 30)
-                Text("Nedd add more")
-            }
+          
             
+        Text("Test")
             
             
         }
