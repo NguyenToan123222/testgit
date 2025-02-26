7714b93c8710255517bc6c652160a7d1b49803e3
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-02-26 18:45:58 +0700

    tes

--- testgit/ContentView.swift
+++ testgit/ContentView.swift
@@ -9,19 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("123")
-        Text("Sign Up")
-            .font(.customfont(.semibold, fontSize: 26))
-            .foregroundColor(.primaryText)
-            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
-            .padding(.bottom, 4)
-        
-        Text("Enter your credentials to continue")
-            .font(.customfont(.semibold, fontSize: 16))
-            .foregroundColor(.secondaryText)
-            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
-            .padding(.bottom, .screenWidth * 0.1)
-        Text("Test")
+       
+        Text("1234")
             
             
         }
