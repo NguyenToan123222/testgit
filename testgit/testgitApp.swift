b6a0957fd108299483e40c28a3d8f893a1591f18
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-02-26 13:53:52 +0700

    Initial Commit

---
+++ testgit/testgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testgitApp.swift
+//  testgit
+//
+//  Created by Nguyễn Toàn on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct testgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
