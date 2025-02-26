97f92a93d5e9f0ff3b361d30fbf31aefc1b54d5d
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-02-26 18:50:00 +0700

    12333

--- testgit/ContentView.swift
+++ testgit/ContentView.swift
@@ -9,10 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       
-        Text("34")
-        Text("3")
-        Text("4")
+     
         Text("1234")
 
             
