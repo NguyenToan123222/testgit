2eb108fd4c79f5a92b2c96a45fb92084a85f36db
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-02-26 14:03:03 +0700

    add more content

--- testgit/ContentView.swift
+++ testgit/ContentView.swift
@@ -11,6 +11,19 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("I love git and xcode")
+            
+            
+            VStack {
+                HStack {
+                    Image(systemName: "circle")
+                        .foregroundColor(.blue)
+                    Text("what's up")
+                }
+                Text("Can come home")
+            }
+            
+            
+            
         }
         .padding()
     }
