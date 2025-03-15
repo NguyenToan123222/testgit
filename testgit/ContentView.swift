b6d5452b6d7b90aef81a2a36966a548bfaafcbb7
NguyenToan123222 <nguyenphutoan25102004@gmail.com>
2025-03-15 19:41:57 +0700

    hii

--- testgit/ContentView.swift
+++ testgit/ContentView.swift
@@ -10,20 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-        HStack{
-            Spacer()
-            Text("Find Products")
-                .font(.customfont(.bold, fontSize: 20))
-                .frame(height: 46)
-            Spacer()
-        }
-        .padding(.top, .topInsets)
-        
-        Text("123")
-        Text ("nh no em")
-    }
+            HStack{
+                Spacer()
+                Text("Find Products")
+                
+                Text ("nh no em")
+            }
         }
     }
+}
 
 
 #Preview {
